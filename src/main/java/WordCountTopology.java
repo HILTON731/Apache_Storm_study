a56f2e0d2b721622aaad7104c015e2daa1cfced5
HILTON731 <hilton3180@gmail.com>
2020-07-30 14:31:01 +0900

    Storm version upgrade

--- src/main/java/WordCountTopology.java
+++ src/main/java/WordCountTopology.java
@@ -1,8 +1,6 @@
-
-import backtype.storm.Config;
-import backtype.storm.LocalCluster;
-import backtype.storm.topology.TopologyBuilder;
-import backtype.storm.tuple.Fields;
+import org.apache.storm.Config;
+import org.apache.storm.LocalCluster;
+import org.apache.storm.topology.TopologyBuilder;
 
 import static java.lang.Thread.sleep;
 
